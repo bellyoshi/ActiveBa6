21cfcd2d13621ecf2367e9a1e97bc6ba8bfb4c92
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-01-13 14:13:14 +0900

    B

---
+++ prototype/BLanguage/BLanguage/CodeBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLanguage
+{
+    internal class CodeBuilder
+    {
+        public void Init()
+        {
+            LoadInstruction()
+        }
+
+        public void LoadInstruction(int space, string value)
+        {
+            AppendCodeLine(space, $"{ value}");
+        }
+    }
+}
