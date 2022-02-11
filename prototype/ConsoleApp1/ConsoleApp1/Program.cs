de65f28404d8fd004db00b288e3b9849c202d99f
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-02-11 11:52:16 +0900

    hello

---
+++ prototype/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = typeof(System.Object);
+            Console.WriteLine(obj.Assembly.FullName);
+            Console.WriteLine(obj.Assembly.ImageRuntimeVersion);
+            Console.WriteLine(obj.AssemblyQualifiedName);
+        }
+    }
+}
--- prototype/metadatabuilder/ConsoleApp1/Program.cs
+++ prototype/metadatabuilder/ConsoleApp1/Program.cs
@@ -1,5 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-var exename = Guid.NewGuid().ToString() + ".exe";
-Console.WriteLine(exename);
-new ConsoleApp1.Class1(exename).BuildHelloWorldApp();
-System.Diagnostics.Process.Start(exename);  
+﻿global using System.Reflection;
+global using System.Reflection.PortableExecutable;
+global using System.Reflection.Metadata;
+global using System.Reflection.Metadata.Ecma335;
+// See https://aka.ms/new-console-template for more information
+//var exename = Guid.NewGuid().ToString() + ".exe";
+//Console.WriteLine(exename);
+//new ConsoleApp1.Class1(exename).BuildHelloWorldApp();
+//System.Diagnostics.Process.Start(exename);  
+ConsoleApp1.Class3.BuildHelloWorldApp();
+System.Diagnostics.Process.Start("ConsoleApplication.exe");  
