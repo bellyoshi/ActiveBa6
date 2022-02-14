c25ebb1f759d9d7f4c2d0073391c19996e680793
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-02-15 08:00:44 +0900

    hello

---
+++ prototype/metadatabuilder/Language/CodeGeneratorVisitor.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language
+{
+    public class CodeGeneratorVisitor:BLanguageBaseVisitor<string>
+    {
+        ConsoleApp1.PEImageCreator PEImageCreator = new ConsoleApp1.PEImageCreator("Hello Wrold.exe");
+    }
+}
